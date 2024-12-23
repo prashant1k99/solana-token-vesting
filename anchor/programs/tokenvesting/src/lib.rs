4c4417bf125589ecd7db636a271c277fd69f7b61
Prashant <prashantco111@gmail.com>
2024-12-23 12:33:52 +0530

    Added employee account creation

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -1,10 +1,133 @@
 #![allow(clippy::result_large_err)]
 
 use anchor_lang::prelude::*;
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
 
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
+
+const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
 pub mod tokenvesting {
     use super::*;
+
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        company_name: String,
+    ) -> Result<()> {
+        // Load in the vesting account as we want to update all the information
+        *ctx.accounts.vesting_account = VestingAccount {
+            owner: ctx.accounts.signer.key(),
+            mint: ctx.accounts.mint.key(),
+            treasury_token_account: ctx.accounts.treasury_token_account.key(),
+            company_name,
+            treasury_bump: ctx.bumps.treasury_token_account,
+            vesting_bump: ctx.bumps.vesting_account,
+        };
+        Ok(())
+    }
+
+    pub fn create_employee_account(
+        ctx: Context<CreateEmployeeAccount>,
+        start_time: u64,
+        end_time: u64,
+        total_amount: u64,
+        cliff_time: u64,
+    ) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            beneficiary: ctx.accounts.beneficiary.key(),
+            start_time,
+            end_time,
+            cliff_time,
+            total_amount,
+            vesting_account: ctx.accounts.vesting_account.key(),
+            total_withdrawn: 0,
+            bump: ctx.bumps.employee_account,
+        };
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    // It is mutable account as it will be paying rent
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR_SIZE + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref()],
+        bump,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        payer = signer,
+        seeds = [b"vesting_treasury", company_name.as_bytes()],
+        bump,
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    // Needed for Account creation
+    pub system_program: Program<'info, System>,
+    // Needed for Token Creation
+    pub token_program: Interface<'info, TokenInterface>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub vesting_bump: u8,
+}
+
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub beneficiary: SystemAccount<'info>,
+
+    #[account(
+        has_one = owner,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    #[account(
+        init,
+        payer = owner,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        space = ANCHOR_DISCRIMINATOR_SIZE + EmployeeAccount::INIT_SPACE,
+        bump,
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+
+    pub system_program: Program<'info, System>,
+}
+
+// Employee Vesting Accounts
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub beneficiary: Pubkey,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub cliff_time: u64,
+    pub vesting_account: Pubkey,
+    pub total_amount: u64,
+    pub total_withdrawn: u64,
+    pub bump: u8,
 }
---
+++ anchor/thinkpad.md
@@ -0,0 +1,6 @@
+### Vesting App for Companies:
+
+Steps:
+1. Initialize Vesting Contract
+2. give employer ability to add employees
+3. Allow employees to claim vested tokens
